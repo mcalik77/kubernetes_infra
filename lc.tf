e4ff880ffd9c5235e8633f54a81ba3e0e19fc9a4
Mustafa Calik <mcalikdevop@gmail.com>
2019-08-17 11:03:17 -0700

    added variable

--- lc.tf
+++ lc.tf
@@ -23,7 +23,7 @@ resource "aws_launch_configuration" "bastions-kubernetesmcalik-com" {
 resource "aws_launch_configuration" "master-us-west-2a-masters-kubernetesmcalik-com" {
   name_prefix                 = "master-us-west-2a.masters.kubernetesmcalik.com-"
   image_id                    = "ami-0d8618ba6320df983"
-  instance_type               = "m4.large"
+  instance_type               = "${var.master_instance_type}"
   key_name                    = "${aws_key_pair.kubernetes-kubernetesmcalik-com-4f94efba1a3c41fb128f0045d0d4cf97.id}"
   iam_instance_profile        = "${aws_iam_instance_profile.masters-kubernetesmcalik-com.id}"
   security_groups             = ["${aws_security_group.masters-kubernetesmcalik-com.id}"]
@@ -46,7 +46,7 @@ resource "aws_launch_configuration" "master-us-west-2a-masters-kubernetesmcalik-
 resource "aws_launch_configuration" "master-us-west-2b-masters-kubernetesmcalik-com" {
   name_prefix                 = "master-us-west-2b.masters.kubernetesmcalik.com-"
   image_id                    = "ami-0d8618ba6320df983"
-  instance_type               = "m4.large"
+  instance_type               = "${var.master_instance_type}"
   key_name                    = "${aws_key_pair.kubernetes-kubernetesmcalik-com-4f94efba1a3c41fb128f0045d0d4cf97.id}"
   iam_instance_profile        = "${aws_iam_instance_profile.masters-kubernetesmcalik-com.id}"
   security_groups             = ["${aws_security_group.masters-kubernetesmcalik-com.id}"]
@@ -69,7 +69,7 @@ resource "aws_launch_configuration" "master-us-west-2b-masters-kubernetesmcalik-
 resource "aws_launch_configuration" "master-us-west-2c-masters-kubernetesmcalik-com" {
   name_prefix                 = "master-us-west-2c.masters.kubernetesmcalik.com-"
   image_id                    = "ami-0d8618ba6320df983"
-  instance_type               = "m4.large"
+  instance_type               = "${var.master_instance_type}"
   key_name                    = "${aws_key_pair.kubernetes-kubernetesmcalik-com-4f94efba1a3c41fb128f0045d0d4cf97.id}"
   iam_instance_profile        = "${aws_iam_instance_profile.masters-kubernetesmcalik-com.id}"
   security_groups             = ["${aws_security_group.masters-kubernetesmcalik-com.id}"]
@@ -92,7 +92,7 @@ resource "aws_launch_configuration" "master-us-west-2c-masters-kubernetesmcalik-
 resource "aws_launch_configuration" "nodes-kubernetesmcalik-com" {
   name_prefix                 = "nodes.kubernetesmcalik.com-"
   image_id                    = "ami-0d8618ba6320df983"
-  instance_type               = "m4.large"
+  instance_type               = "${var.node_instance_type}"
   key_name                    = "${aws_key_pair.kubernetes-kubernetesmcalik-com-4f94efba1a3c41fb128f0045d0d4cf97.id}"
   iam_instance_profile        = "${aws_iam_instance_profile.nodes-kubernetesmcalik-com.id}"
   security_groups             = ["${aws_security_group.nodes-kubernetesmcalik-com.id}"]
---
+++ variable.tf
@@ -0,0 +1,6 @@
+variable "master_instance_type" {
+  
+}
+variable "node_instance_type" {
+  
+}
